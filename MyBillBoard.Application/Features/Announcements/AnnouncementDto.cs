fd9bc03277fd1da4035467133560fd10d3857ef0
Marian Sabov <marian.sabov@gmail.com>
2026-02-03 23:54:15 +0200

    stard dev new feature

---
+++ MyBillBoard.Application/Features/Announcements/AnnouncementDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBillBoard.Application.Features.Announcements
+{
+    internal class AnnouncementDto
+    {
+    }
+}
--- MyBillBoard.Application/obj/Debug/net10.0/MyBillBoard.Application.AssemblyInfo.cs
+++ MyBillBoard.Application/obj/Debug/net10.0/MyBillBoard.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBillBoard.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f926ad64660c9ac381000cafd3616b7fd1d9f805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b04d576c05a9c46edd2a1a390110056252c0525a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBillBoard.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBillBoard.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
